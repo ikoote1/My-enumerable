4592760295a4b2b9ad89ed5238f8292bb5289ebd
ikoote1 <ikootepreim@gmail.com>
2023-05-12 11:13:25 +0300

    enumerable

--- enumerable.rb
+++ enumerable.rb
@@ -1,23 +1,22 @@
+require_relative 'my_module'
 
-# require_relative 'my_enumerable'
+class MyList
+  include MyEnumerable
+  def initialize(args)
+    @list = []
+    args.each { |arg| @list << arg }
+  end
 
-# class MyList
-#   include MyEnumerable
-#   def initialize(args)
-#     @list = []
-#     args.each { |arg| @list << arg }
-#   end
+  def each(&block)
+    @list.each(&block)
+  end
+end
 
-#   def each(&block)
-#     @list.each(&block)
-#   end
-# end
+list = MyList.new([1, 2, 3, 4])
+puts(list.all? { |e| e < 5 })
+puts(list.all? { |e| e > 5 })
 
-# list = MyList.new([1, 2, 3, 4])
-# puts(list.all? { |e| e < 5 })
-# puts(list.all? { |e| e > 5 })
+puts(list.any? { |e| e == 2 })
+puts(list.any? { |e| e == 5 })
 
-# puts(list.any? { |e| e == 2 })
-# puts(list.any? { |e| e == 5 })
-
-# puts list.filter(&:even?)
+puts list.filter(&:even?)

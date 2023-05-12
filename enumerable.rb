0c2e9850eb92c7fdcfd7d405559ac2676dfec92c
ikoote1 <ikootepreim@gmail.com>
2023-05-12 11:11:39 +0300

    enumerable

--- enumerable.rb
+++ enumerable.rb
@@ -1,40 +1,4 @@
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# # # prem
-# module MyEnumerable
-#     def all?(&block)
-#       result = true
-#       each { |elem| result = false unless block.call(elem) }
-#       result
-#     end
-  
-#     def any?(&block)
-#       result = false
-#       each { |elem| result = true if block.call(elem) }
-#       result
-#     end
-  
-#     def filter(&block)
-#       result = []
-#       each { |elem| result.push(elem) if block.call(elem) }
-#       result
-#     end
-#   end
 # require_relative 'my_enumerable'
 
 # class MyList
